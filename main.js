5bbfc14b71841ad50baf2d95629f68583b2ae68f
Shoyunus1221 <157447196+Shoyunus1221@users.noreply.github.com>
2024-03-08 17:57:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+setTimeout(() =>{
+    body.classList.remove("bg-[url('https://c4.wallpaperflare.com/wallpaper/54/119/802/technology-instagram-social-media-hd-wallpaper-preview.jpg')]");
+    loading.classList.add('hidden');
+    body.classList.add("bg-[url('https://t4.ftcdn.net/jpg/06/55/50/69/360_F_655506911_1p13PsGKM93SuCePkLUcd75TW1Bzu4jg.jpg')]");
+    login.classList.remove('hidden');
+}, 5000);
